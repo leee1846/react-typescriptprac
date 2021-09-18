f31ade2c6f54e275a43c55aa7c5fb9e0d2fbff8f
leee1846 <leee1846@gmail.com>
2021-09-18 18:07:44 +0900

    style: navigation 스타일 적용

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import * as S from './style';
 
 const Navigation = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  const navigationList = [
+    { id: 1, name: 'Alpha' },
+    { id: 2, name: 'Bravo' },
+    { id: 3, name: 'Charlie' },
+    { id: 4, name: 'Delta' },
+    { id: 5, name: 'Echo' },
+    { id: 6, name: 'Result' },
+  ];
+
   return (
     <S.Nav>
       <S.Ul>
-        <S.Li>
-          <Link to="/alpha">Alpha</Link>
-        </S.Li>
-        <S.Li>
-          <Link to="/bravo">Bravo</Link>
-        </S.Li>
-        <S.Li>
-          <Link to="/charlie">Charlie</Link>
-        </S.Li>
-        <S.Li>
-          <Link to="/delta">Delta</Link>
-        </S.Li>
-        <S.Li>
-          <Link to="/echo">Echo</Link>
-        </S.Li>
-        <S.Li>
-          <Link to="/result">Result</Link>
-        </S.Li>
+        {navigationList.map((navItem) => (
+          <S.Li
+            key={navItem.id}
+            isClicked={currentPath.includes(navItem.name.toLowerCase())}
+          >
+            <Link to={`/${navItem.name.toLowerCase()}`}>{navItem.name}</Link>
+          </S.Li>
+        ))}
       </S.Ul>
     </S.Nav>
   );
--- src/components/Navigation/style.ts
+++ src/components/Navigation/style.ts
@@ -16,17 +16,29 @@ export const Ul = styled.ul`
   align-items: center;
 `;
 
-export const Li = styled.li`
+interface LiProps {
+  isClicked: boolean;
+}
+export const Li = styled.li<LiProps>`
   height: 100%;
   width: 110px;
   margin-right: 20px;
 
   & > a {
+    font-family: ${({ isClicked }) => isClicked && 'Lato Bold'};
     display: flex;
     align-items: center;
     justify-content: center;
     width: 100%;
     height: 100%;
     font-size: 20px;
+    text-shadow: ${({ isClicked }) =>
+      isClicked && '1px 4px 4px rgba(0, 0, 0, 0.25)'};
+    transition: 0.3s;
+
+    &:hover {
+      font-family: 'Lato Bold';
+      text-shadow: 1px 4px 4px rgba(0, 0, 0, 0.25);
+    }
   }
 `;
