79eebc67bbd10c5d7ca6063c2d5ddc771d1f8886
leee1846 <leee1846@gmail.com>
2021-09-20 00:36:11 +0900

    feat: swr 초기options 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { SWRConfig } from 'swr';
 import ResetStyle from './styles/ResetStyle';
 import Layout from './styles/Layout';
 import Header from './components/Commons/Header';
@@ -7,13 +8,15 @@ import RootPage from './pages/Root';
 
 function App() {
   return (
-    <Router>
-      <ResetStyle />
-      <Header />
-      <Layout>
-        <RootPage />
-      </Layout>
-    </Router>
+    <SWRConfig value={{ revalidateOnFocus: false }}>
+      <Router>
+        <ResetStyle />
+        <Header />
+        <Layout>
+          <RootPage />
+        </Layout>
+      </Router>
+    </SWRConfig>
   );
 }
 
